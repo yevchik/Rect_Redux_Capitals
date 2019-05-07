2adaccf2e5ebb192ceffe6842eaf299adaf8e919
yevchik <yevchyk5@gmail.com>
2019-05-07 19:19:03 +0300

    Lust Change

---
+++ build/precache-manifest.48ce9594b4b33c1145dec69412077dab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "677ce3e037b691a13a60",
+    "url": "/static/js/main.b16a9031.chunk.js"
+  },
+  {
+    "revision": "c992859a8cbc38e63114",
+    "url": "/static/js/2.a2e09441.chunk.js"
+  },
+  {
+    "revision": "677ce3e037b691a13a60",
+    "url": "/static/css/main.ca9731fd.chunk.css"
+  },
+  {
+    "revision": "85285ce78d570049b6f858e8e7818cbb",
+    "url": "/index.html"
+  }
+];
